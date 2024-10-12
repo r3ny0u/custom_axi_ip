d5d4cb8573c3dd1ae0ab1a5ff7fd7aeb75fea9c2
r3ny0u <renyou14@gmail.com>
2024-10-12 13:35:30 +0800

    reorder the sequence and update 2 data reg

--- rtl/custom_axi_ip.sv
+++ rtl/custom_axi_ip.sv
@@ -18,13 +18,15 @@ module custom_axi_ip
   logic [63:0] internal_data;
   status_e state;
 
-  always_ff @(posedge clk_i or negedge rst_ni) begin
+  always_ff @(posedge clk_i) begin
     if (!rst_ni) begin
       internal_data <= 64'b0;
       state <= custom_axi_ip_pkg::IDLE;
     end else begin
       case (state)
         custom_axi_ip_pkg::IDLE: begin
+          wen_out <= 1'b0;
+          status_out <= custom_axi_ip_pkg::IDLE;
           if (enable_in) begin
             $display("Idle state");
             internal_data <= ipreg_data;
@@ -32,29 +34,30 @@ module custom_axi_ip
           end else begin
             state <= custom_axi_ip_pkg::IDLE;
           end
-          wen_out <= 1'b0;
-          status_out <= custom_axi_ip_pkg::IDLE;
         end
         custom_axi_ip_pkg::BUSY: begin
-          $display("Busy state");
-          internal_data <= internal_data + 1;
-          state <= custom_axi_ip_pkg::DONE;
           wen_out <= 1'b0;
           status_out <= custom_axi_ip_pkg::BUSY;
+          $display("Busy state");
+          internal_data[63:32] <= internal_data + 1;
+          internal_data[31:0] <= internal_data + 1;
+          $display("Internal data 1: %d", internal_datap[63:32]);
+          $display("Internal data 2: %d", internal_datap[31:0]);
+          state <= custom_axi_ip_pkg::DONE;
         end
         custom_axi_ip_pkg::DONE: begin
+          wen_out <= 1'b1;
+          status_out <= custom_axi_ip_pkg::DONE;
           $display("Done state");
           ipreg_data_out <= internal_data;
           enable_out <= 1'b0;
           state <= custom_axi_ip_pkg::IDLE;
-          wen_out <= 1'b1;
-          status_out <= custom_axi_ip_pkg::DONE;
         end
         custom_axi_ip_pkg::ERROR: begin
-          $display("Error state");
           wen_out <= 1'b0;
-          state <= custom_axi_ip_pkg::IDLE;
           status_out <= custom_axi_ip_pkg::ERROR;
+          $display("Error state");
+          state <= custom_axi_ip_pkg::IDLE;
         end
         default: begin
           state <= custom_axi_ip_pkg::ERROR;
