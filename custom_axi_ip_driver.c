48021588bb8920ead704e93e857232557550f69a
r3ny0u <renyou14@gmail.com>
2024-10-31 14:03:41 +0800

    updated hjson with hwext

--- custom_axi_ip_driver.c
+++ custom_axi_ip_driver.c
@@ -3,11 +3,12 @@
 #include <custom_axi_ip_driver.h>
 #include <custom_axi_ip.h>
 
-#define BASE_ADDR 0x1A400000
+#define BASE_ADDR 0x1A1400000
 
 void write_data(uint32_t *din) {
     uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_DIN_REG_OFFSET);
-    *reg = *din;
+    reg = din;
+    // reg[0] = din[0];
 }
 
 void read_data(uint32_t *dout) {
@@ -29,6 +30,7 @@ void wait() {
 }
 
 void start_test (uint32_t din[1], uint32_t dout[1]) {
+    printf("Starting test in driver");
     write_data(din);
     enable();
     wait();
