069151833e8b2c4422cf55a97eed1e1bfaf7a7ae
r3ny0u <renyou14@gmail.com>
2024-10-31 16:57:05 +0800

    remove custom_axi_ip to check if that is the issue

--- rtl/custom_axi_ip_top.sv
+++ rtl/custom_axi_ip_top.sv
@@ -72,15 +72,15 @@ module custom_axi_ip_top
     // wiring signals between control unit and custom axi ip
     // logic [1:0] enable;
 
-    custom_axi_ip i_custom_axi_ip (
-        .clk_i(clk_i),
-        .rst_ni(rst_ni),
-        .din(reg_file_to_ip.din.q),
-        .enable_in(reg_file_to_ip.enable.q),
-        .dout(ip_to_reg_file.dout.d),
-        .enable_out(ip_to_reg_file.enable),
-        .status_out(ip_to_reg_file.status.d)
-    );
+    // custom_axi_ip i_custom_axi_ip (
+    //     .clk_i(clk_i),
+    //     .rst_ni(rst_ni),
+    //     .din(reg_file_to_ip.din.q),
+    //     .enable_in(reg_file_to_ip.enable.q),
+    //     .dout(ip_to_reg_file.dout.d),
+    //     .enable_out(ip_to_reg_file.enable.d),
+    //     .status_out(ip_to_reg_file.status.d)
+    // );
 
     // assign ip_to_reg_file.enable.d = enable[1];
     // assign ip_to_reg_file.enable.de = enable[0];
